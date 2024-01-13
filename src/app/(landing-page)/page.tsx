311f1de3e1728d4b72d605fc8d2644936ccde7b4
Amlan Roy <amlanroy2500@gmail.com>
2024-01-13 16:33:42 +0530

    Added third section in landing page

--- src/app/(landing-page)/page.tsx
+++ src/app/(landing-page)/page.tsx
@@ -1,9 +1,38 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import React from "react";
-import a from "../../../public/resume-illustration.png";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { FaRegClock, FaRegFileAlt, FaCodeBranch } from "react-icons/fa";
 
 type LandingPageProps = {};
+
+const FeaturesCard = ({
+  icon,
+  title,
+  description,
+}: {
+  icon: React.ReactNode;
+  title: string;
+  description: string;
+}) => {
+  return (
+    <div className="w-72">
+      <div className="z-20 flex justify-center items-center rounded-full md:w-16 md:h-16 w-10 h-10 bg-brand-primary-green-10 text-brand-neutral-2 relative top-5 md:left-[calc(50%-32px)] left-[calc(50%-20px)]">
+        {icon}
+      </div>
+      <Card className="p-2 bg-brand-secondary-blue-4 md:min-h-72 min-h-64">
+        <CardHeader>
+          <CardTitle className="text-center text-brand-primary-green-10 md:text-2xl text-xl font-bold">
+            {title}
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p className="text-brand-primary-green-8 text-base">{description}</p>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
 
 const LandingPage: React.FC<LandingPageProps> = () => {
   return (
@@ -88,6 +117,38 @@ const LandingPage: React.FC<LandingPageProps> = () => {
               Craft transforms it into a tailor-made, ATS-friendly resume.
             </p>
           </div>
+        </div>
+      </section>
+      <section className="max-w-screen-xl overflow-hidden px-4 sm:px-6 mt-24 sm:flex sm:flex-col md:justify-center md:items-start items-center md:mx-auto mx-6">
+        <h2 className="md:text-5xl text-4xl font-bold text-brand-neutral-11 mx-auto text-center">
+          Key Features
+        </h2>
+        <h5 className="md:text-2xl text-base text-brand-neutral-8 mt-6 text-center mx-auto">
+          Unlock Your Potential with Resume Craft's Powerful Features
+        </h5>
+
+        <div className="flex flex-wrap md:gap-12 gap-8 mx-auto items-center justify-center">
+          <FeaturesCard
+            icon={<FaRegClock size={20} />}
+            title={"Automation"}
+            description={
+              "Cut down hours of manual tweaking. Resume Craft automates the customization process, allowing you to focus on what matters – your skills and experience."
+            }
+          />
+          <FeaturesCard
+            icon={<FaRegFileAlt size={20} />}
+            title={"ATS Optimization"}
+            description={
+              "Our system is designed to enhance your resume's compatibility with Applicant Tracking Systems, increasing your chances of getting noticed."
+            }
+          />
+          <FeaturesCard
+            icon={<FaCodeBranch size={20} />}
+            title={"Versatility"}
+            description={
+              "Tailor your resume for diverse job opportunities effortlessly. Resume Craft adapts to different roles, industries, and requirements."
+            }
+          />
         </div>
       </section>
     </>
