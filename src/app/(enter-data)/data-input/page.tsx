b4fd457b07faf2942893618f6b3e2da5d29bca80
Amlan Roy <59330872+amlan-roy@users.noreply.github.com>
2024-01-17 22:38:14 +0530

    Added hovercard and link in data-input page

--- src/app/(enter-data)/data-input/page.tsx
+++ src/app/(enter-data)/data-input/page.tsx
@@ -19,6 +19,12 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
 } from "@/components/ui/alert-dialog";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
+import Link from "next/link";
 
 type EnterDataPageProps = {};
 
@@ -88,15 +94,58 @@ const EnterDataPage: React.FC<EnterDataPageProps> = () => {
         <p className="text-brand-neutral-10 text-center text-2xl mt-11">
           This data will be used to generate the customised resume for different
           job types. Fill in this detail as accurately as possible. You can
-          refer this
-          <a
+          refer this{" "}
+          <Link
+            href={
+              "https://cdn-careerservices.fas.harvard.edu/wp-content/uploads/sites/161/2023/08/College-resume-and-cover-letter-4.pdf"
+            }
             target="_blank"
-            href="https://cdn-careerservices.fas.harvard.edu/wp-content/uploads/sites/161/2023/08/College-resume-and-cover-letter-4.pdf"
             aria-label="Visit the external link to harvard resume guide"
+            className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
           >
-            {" Harvard Resume Guide "}
-          </a>
-          and google X-Y-Z format for reference.
+            {"Harvard Resume Guide"}
+          </Link>{" "}
+          and{" "}
+          <HoverCard>
+            <HoverCardTrigger asChild>
+              <Button
+                variant="link"
+                className="text-brand-neutral-10 text-center text-2xl m-0 px-0"
+              >
+                Google's X-Y-Z format
+              </Button>
+            </HoverCardTrigger>
+            <HoverCardContent className="w-96">
+              <div className="flex justify-between space-x-4">
+                <div className="space-y-1">
+                  <h4 className="text-sm font-semibold">
+                    Google's X-Y-Z format
+                  </h4>
+                  <p className="text-sm">
+                    The Google resume x y z format is a way of writing your
+                    resume that emphasizes your accomplishments and skills using
+                    a simple formula.
+                    <br />
+                    <br />
+                    The formula is: “Accomplished [X] as measured by [Y], by
+                    doing [Z].”
+                    <br />
+                    <br />
+                    For example, “Managed a community support program (X) that
+                    saved $2 million (Y), by designing a streamlined customer
+                    service portal (Z).”
+                    <br />
+                    <br />
+                    This format helps you showcase your achievements and how
+                    they relate to the job you are applying for. It also makes
+                    it easier for recruiters to understand your value and
+                    potential.
+                  </p>
+                </div>
+              </div>
+            </HoverCardContent>
+          </HoverCard>{" "}
+          for reference.
         </p>
       </section>
       <div className="max-w-screen-xl overflow-hidden px-4 sm:px-6 mt-10 mx-auto mb-28">
