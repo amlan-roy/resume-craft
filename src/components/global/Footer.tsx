7872b94f8676d1d983a8f029f9a21daccdd45c2c
Amlan Roy <59330872+amlan-roy@users.noreply.github.com>
2024-01-20 03:35:00 +0530

    Some changes

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -1,38 +1,10 @@
 import React from "react";
 import Logo from "@/components/global/Logo";
-import {
-  FaHeart,
-  FaGithub,
-  FaLinkedin,
-  FaTwitter,
-  FaGlobeAsia,
-} from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
 import Link from "next/link";
+import { LINKS } from "@/lib/const/components/global/footer";
 
 type FooterProps = {};
-
-const LINKS = [
-  {
-    url: "https://amlan-roy.github.io/",
-    description: "Amlan Roy's Portfolio Website's Link",
-    icon: <FaGlobeAsia size={32} className="w-4 h-4 md:w-6 md:h-6" />,
-  },
-  {
-    url: "https://github.com/amlan-roy/",
-    description: "Amlan Roy' GitHub Link",
-    icon: <FaGithub size={32} className="w-4 h-4 md:w-6 md:h-6" />,
-  },
-  {
-    url: "https://www.linkedin.com/in/amlan-roy/",
-    description: "Amlan Roy's LinkedIn Link",
-    icon: <FaLinkedin size={32} className="w-4 h-4 md:w-6 md:h-6" />,
-  },
-  {
-    url: "https://twitter.com/_royamlan_",
-    description: "Amlan Roy's Twitter Link",
-    icon: <FaTwitter size={32} className="w-4 h-4 md:w-6 md:h-6" />,
-  },
-];
 
 const Footer: React.FC<FooterProps> = () => {
   return (
