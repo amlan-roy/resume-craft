276074b63f5a739b93319ba488269ecb8ddd2301
Amlan Roy <59330872+amlan-roy@users.noreply.github.com>
2024-02-21 09:49:01 +0530

    fix:updated the env variables and its usage

--- src/lib/utils/firebase/firebase-admin-config.ts
+++ src/lib/utils/firebase/firebase-admin-config.ts
@@ -12,7 +12,8 @@ const firebaseAdminConfig = {
   credential: cert({
     projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
     clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+    privateKey: JSON.parse(process.env.FIREBASE_ADMIN_PRIVATE_KEY || "{}")
+      .value,
   }),
 };
 
