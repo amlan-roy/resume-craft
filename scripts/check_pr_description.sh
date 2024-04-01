b6411afe318eb39670e29476d762294cada1837b
Amlan Roy <59330872+amlan-roy@users.noreply.github.com>
2024-04-01 16:16:56 +0530

    Updated the script

--- scripts/check_pr_description.sh
+++ scripts/check_pr_description.sh
@@ -8,10 +8,10 @@ if ! echo "$description" | grep -q '## Summary'; then
 elif ! echo "$description" | grep -q '## Testing done'; then
   echo "PR description is missing testing done section"
   exit 1
-elif ! echo "$description" | awk '/## Summary/{flag=1;next}/##/{flag=0}flag' | awk 'length($0) < 5 && NR > 1'; then
+elif ! echo "$description" | awk '/## Summary/{flag=1;next}/##/||/#/{flag=0}END{flag=0}flag' | awk 'length($0) < 5 && NR > 1'; then
   echo "Summary section has less than 5 characters"
   exit 1
-elif ! echo "$description" | awk '/## Testing done/{flag=1;next}/##/{flag=0}flag' | awk 'length($0) < 5 && NR > 1'; then
+elif ! echo "$description" | awk '/## Testing done/{flag=1;next}/##/||/#/{flag=0}END{flag=0}flag' | awk 'length($0) < 5 && NR > 1'; then
   echo "Testing section has less than 5 characters"
   exit 1
 fi
